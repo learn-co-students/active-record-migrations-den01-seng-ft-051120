a3a69da942f0bc2d5ebab31114f74fb3613b833e
github username <github email address>
2020-05-25 11:12:47 -0600

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateArtists < ActiveRecord::Migration[4.2]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
